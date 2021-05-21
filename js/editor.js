3176743366620b18c3affde84f241c2735272260
SpicyPTV <pietered12@gmail.com>
2021-05-21 18:43:00 +0100

    Major JS Changes (Adding comments, simplifying code)

--- js/editor.js
+++ js/editor.js
@@ -1,109 +1,118 @@
+// Variable Bank
 var searchCntrl = false;
-var subtitle = true;
-var currentPage = 0;
-var basedOn = true;
-var coverstatus = true;
-var fomCntrl = false;
-var totalPages = 0;
-var zoom = 1;
-window.onload=function() {
-  totalPages = $('div#page').length;
-  $("#pageCount").html(currentPage + "/" + totalPages);
-  $("#cover-settings").hide();
+var subtitle = true; // Controls wether subtitle is active or not
+var currentPage = 0; // Contains the current page number
+var basedOn = true; // Controls wether the based on section is active or not
+var coverstatus = true; // Controls wether cover is shown or not
+var fomCntrl = false; // Controls format menu state
+var totalPages = null; // Contains the total amount of pages
+var zoom = 1; // Contains current zoom value
+
+window.onload = function() {
+  $("#cover-settings").hide(); // Hides cover settings menu
+  totalPages = $('div#page').length; // Gets amount of pages
+  $("#pageCount").html(currentPage + "/" + totalPages); // Prints amount of pages
 }
+
 function zoomIn() {
-  zoom+=0.1;
-  $("#pages-container").css("transform", "scale("+zoom+")");
-  console.log(zoom);
+  zoom += 0.1; // Increases zoom by 10%
+  $("#pages-container").css("transform", "scale("+zoom+")"); // Applys zoom value
 }
+
 function zoomOut() {
-  zoom-=0.1;
-  if(zoom<=0.1) {
-    zoom=0.1;
-    $("#pages-container").css("transform", "scale("+zoom+")");
-  }
-  console.log(zoom);
-  $("#pages-container").css("transform", "scale("+zoom+")");
+  zoom -= 0.1; // Decreases zoom by 10%
+  if (zoom <= 0.1) { zoom = 0.1; } // Catches Zoom so it doesnt get smaller than 0.1
+  $("#pages-container").css("transform", "scale("+zoom+")"); // Applys zoom value
 }
+
+// Controls moving buttons, updates every ¼ second
 setInterval(function() {
-  if(zoom!=1) {
-    $("#undo-background").css("right", "3.5vw");
-  } else if (zoom==1) {
-    $("#undo-background").css("right", "-3.5vw");
+  // Controls reset buttons position
+  if (zoom != 1) {
+    $("#undo-background").css("right", "3.5vw"); // Reset button on screen
+  } else if (zoom == 1) {
+    $("#undo-background").css("right", "-3.5vw"); // Reset button of screen
   }
-  if(coverstatus===false) {
-    $("#coverButton").css("right","4vw")
-  } else if(coverstatus===true) {
-    $("#coverButton").css("right","-3vw")
+  // Controls cover buttons position
+  if (coverstatus === false) {
+    $("#coverButton").css("right","4vw") // Cover button on screen
+  } else if (coverstatus === true) {
+    $("#coverButton").css("right","-3vw") // Cover button off screen
   }
 },250)
+
 function resetZoom() {
-  zoom = 1;
-  $("#pages-container").css("transform", "scale("+zoom+")");
-  $("#reset-background").css("right", "-3.5vw");
-  console.log(zoom);
+  zoom = 1; // Sets zoom to 100%
+  $("#pages-container").css("transform", "scale("+zoom+")"); // Applys zoom value
+  $("#reset-background").css("right", "-3.5vw"); // Puts reset button of screen
 }
+
 function searchScript() {
-  if(searchCntrl===false) {
-    $("#search-area").css("width", "10vw");
-    $("#search-area").css("right", "2.85vw");
-    $("#search-background").css("border-radius", "0 15% 15% 0");
-    searchCntrl = true;
-  } else if(searchCntrl===true) {
-    $("#search-area").css("width", "0");
-    $("#search-area").css("right", "2vw");
-    $("#search-background").css("border-radius", "15%");
-    searchCntrl = false;
+  if (searchCntrl === false) {
+    $("#search-area").css("width", "10vw"); // Extends search box
+    $("#search-area").css("right", "2.85vw"); // Moves the search box slightly (To hide some overlap)
+    $("#search-background").css("border-radius", "0 15% 15% 0"); // Flattens search button edges
+    searchCntrl = true; // Tells function the search box is open
+  } else if (searchCntrl === true) {
+    $("#search-area").css("width", "0"); // Closes search box
+    $("#search-area").css("right", "2vw"); // Moves  search box slightly (To hide some overlap)
+    $("#search-background").css("border-radius", "15%"); // Smooths search button edges
+    searchCntrl = false; // Tells function the search box is closed
   }
 }
+
 function coverSettingsCntrl() {
-  $("#cover-settings").slideToggle(500);
-  console.log("yes");
+  $("#cover-settings").slideToggle(500); // Slides cover settings menu
 }
+
 function toggleSub() {
-  if(subtitle===true) {
-    $("#subtitle").hide();
-    $("#title").css("top","44.5%")
-    $("#button1").html("Off")
-    subtitle = false;
+  if (subtitle === true) {
+    $("#subtitle").hide(); // Hides subtitle
+    $("#title").css("top","44.5%") // Moves title down
+    $("#button1").html("Off") // Makes button value 'Off'
+    subtitle = false; // Tells function subtitle is hidden
   } else if (subtitle===false) {
-    $("#subtitle").show();
-    $("#title").css("top","38.5%")
-    $("#button1").html("On")
-    subtitle = true;
+    $("#subtitle").show(); // Shows subtitle
+    $("#title").css("top","38.5%") // Moves title up
+    $("#button1").html("On") // Makes button value 'On'
+    subtitle = true; // Tells function subtitle is visible
   }
 }
+
 function toggleBasedOn() {
-  if(basedOn===true) {
-    $("#basedOn").hide();
-    $("#button2").html("Off")
-    basedOn = false;
-  } else if (basedOn===false) {
-    $("#basedOn").show();
-    $("#button2").html("On")
-    basedOn = true;
+  if (basedOn === true) {
+    $("#basedOn").hide(); // Hides based on section
+    $("#button2").html("Off") // Makes button value 'Off'
+    basedOn = false; // Tells function the based on section is hidden
+  } else if (basedOn === false) {
+    $("#basedOn").show(); // Shows based on section
+    $("#button2").html("On") // Makes button value 'On'
+    basedOn = true; // Tells function the based on section is visible
   }
 }
+
 function coverHide() {
-  $("#cover").hide();
-  coverstatus = false;
+  $("#cover").hide(); // Hides cover
+  coverstatus = false; // Tells function the cover is hidden
 }
+
 function coverShow() {
-  $("#cover").show();
-  coverstatus = true;
+  $("#cover").show(); // Shows cover
+  coverstatus = true; // Tells function the cover is visible
 }
+
 function fom() {
-  if(fomCntrl===false) {
-    $("#fom").css("height","7.5vw");
-    $("#fom").css("right","2.9vw");
-    $("#fom").css("width","10vw");
-    $("#formating-icon").css("border-radius", "0 15% 15% 0");
-    fomCntrl = true;
-  } else if(fomCntrl===true) {
-    $("#fom").css("height","0");
-    $("#fom").css("width","0");
-    $("#fom").css("right","2.5vw");
-    $("#formating-icon").css("border-radius", "15%");
-    fomCntrl = false;
+  if(fomCntrl === false) {
+    $("#fom").css("height","7.5vw"); // Sets height
+    $("#fom").css("right","2.9vw"); // Sets right position
+    $("#fom").css("width","10vw"); // Sets width
+    $("#formating-icon").css("border-radius", "0 15% 15% 0"); // Flattens button edges
+    fomCntrl = true; // Tells function the menu is open
+  } else if(fomCntrl === true) {
+    $("#fom").css("height","0"); // Sets height
+    $("#fom").css("right","2.5vw"); // Sets right position
+    $("#fom").css("width","0"); // Sets width
+    $("#formating-icon").css("border-radius", "15%"); // Smooths button edges
+    fomCntrl = false; // Tells function the menu is closed
   }
 }
