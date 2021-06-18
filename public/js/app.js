b4484b57f8abe2ee6b418c74c8dcf15d408754dd
SpicyPTV <PieterEd12@gmail.com>
2021-06-18 16:17:24 +0100

    Setup Sign-In functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
-console.log(firebase);
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('main');
+const whenSignedOut = document.getElementById('log-in');
+
+const signInBtn = document.getElementById('signInBtn');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        alert(user.photoURL)
+        $(".rightSideTools").append('<img src="'+user.photoURL+'" id="accountLogo">')
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+    }
+});
