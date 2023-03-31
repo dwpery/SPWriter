8b964116eef6f04b8fd72e5ecd4804074cc3ee95
SpicyPTV <pietered12@gmail.com>
2023-03-31 23:58:35 +0100

    New start

--- public/main.js
+++
@@ -1,7 +0,0 @@
-var totalNotes = 0;
-
-function addNote() {
-    totalNotes++;
-    $(".noteSelection").append('<div class="noteName" id="' + totalNotes + '">Note #' + totalNotes + '</div>');
-    $(".noteSpace").append('<div id="' + totalNotes + '"><div class="writingSpace" contenteditable></div></div>')
-}
